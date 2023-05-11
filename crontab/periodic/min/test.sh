db5746992b7644a19c2ca4ac3a12a35d1369c6d6
jinlin <jinlin.mu@pandu.ie>
2023-05-11 19:03:18 +0800

    update test.sh for crontab

--- crontab/periodic/min/test.sh
+++ crontab/periodic/min/test.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] testing" >> /app/log/$(date +%Y-%m-%d).log
+job_name="test"
+job_dir="/app/log/${job_name}"
+if [ -d "${job_dir}" ]; then
+#    echo "Folder '${your_folder}' already exists"
+else
+    mkdir "${job_dir}"
+fi
 
-#curl --request GET -sL \
-#     --url 'http://www.baidu.com'\
-#     --output '/app/log/test.log'
+today=$(date +%Y-%m-%d)
+log_file="${job_dir}/${today}.log"
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] test finished" >> /app/log/$(date +%Y-%m-%d).log
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] testing" >> ${log_file}
+
+#curl_output=$(curl --request GET -sL \
+#                    --url 'https://www.baidu.com'\
+#                    -w '%{http_code}\n')
+#echo "[$(date '+%Y-%m-%d %H:%M:%S')] curl output: ${curl_output}" >> ${log_file}
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] test finished" >> ${log_file}
 echo "" >> /app/log/$(date +%Y-%m-%d).log
